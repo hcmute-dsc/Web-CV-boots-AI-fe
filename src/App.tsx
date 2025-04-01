9132a599277654b42a44ef0bbaa9ff221ef5b255
Quach Tai LOi <tailoi1606@gmail.com>
2025-04-01 23:31:26 +0700

    Update-file-structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
-import Nvbar from '../components/Nvbar'
-import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from "../pages/Home";
-import Tips from "../pages/CVTips";
-import FAQ from "../pages/FAQ";
-import Generate from "../pages/CVGenerate";
-import Samples from "../pages/CVSamples";
+import Nvbar from "./components/Nvbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Tips from "./pages/CVTips";
+import FAQ from "./pages/FAQ";
+import Generate from "./pages/CVGenerate";
+import Samples from "./pages/CVSamples";
 
 const App = () => {
   return (
     <div>
       <Router>
-        <Nvbar/>
+        <Nvbar />
         <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/cvgenerate" element={<Generate />} />
-        <Route path="/cvsamples" element={<Samples />} />
-        <Route path="/cvtips" element={<Tips />} />
-        <Route path="/faq" element={<FAQ />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/cvgenerate" element={<Generate />} />
+          <Route path="/cvsamples" element={<Samples />} />
+          <Route path="/cvtips" element={<Tips />} />
+          <Route path="/faq" element={<FAQ />} />
         </Routes>
       </Router>
-      
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
