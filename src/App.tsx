ec0c5d4d37a7387e2f06c1444bd0def358eca771
TinMH <tinhongmai1012@gmail.com>
2025-04-02 18:19:45 +0700

    Add header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,25 @@ import Tips from "./pages/CVTips";
 import FAQ from "./pages/FAQ";
 import Generate from "./pages/CVGenerate";
 import Samples from "./pages/CVSamples";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div>
       <Router>
-        <Nvbar />
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/cvgenerate" element={<Generate />} />
-          <Route path="/cvsamples" element={<Samples />} />
-          <Route path="/cvtips" element={<Tips />} />
-          <Route path="/faq" element={<FAQ />} />
-        </Routes>
+        <div className="flex flex-col bg-white min-h-screen">
+          <Nvbar />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/home" element={<Home />} />
+              <Route path="/cvgenerate" element={<Generate />} />
+              <Route path="/cvsamples" element={<Samples />} />
+              <Route path="/cvtips" element={<Tips />} />
+              <Route path="/faq" element={<FAQ />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
       </Router>
     </div>
   );
--- src/components/Nvbar.tsx
+++ src/components/Nvbar.tsx
@@ -1,23 +1,51 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import logo from "../assets/logo_dsc.png";
 
 const Nvbar = () => {
-    return(
-        <nav className="z-50">
-            <div className="h-10vh flex justify-between lg:py-5 px-20 py-20 border-b">
-                <div className="flex items-center flex-1">
-                    <h2 className="text-3xl font-bold text-black-500">CV Booster</h2>
-                </div>
-                <div>
-                    <ul className="flex gap-8 mr-16 text-[18px] text-gray-400">
-                        <Link to='/home'><li className="hover:text-blue-400 border-white cursor-pointer">Trang chủ</li></Link>
-                        <Link to='/cvgenerate'><li className="hover:text-blue-400 border-white cursor-pointer">Tạo CV</li></Link>
-                        <Link to='/cvsamples'><li className="hover:text-blue-400 border-white cursor-pointer">Mẫu CV</li></Link>
-                        <Link to='/cvtips'><li className="hover:text-blue-400 border-white cursor-pointer">Mẹo viết CV</li></Link>
-                        <Link to='/faq'><li className="hover:text-blue-400 border-white cursor-pointer">FAQ</li></Link>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    )
-}
-export default Nvbar
+  return (
+    <nav className="z-50">
+      <div className="h-10vh flex justify-between lg:py-5 px-20 py-20 border-b-1 border-b-black">
+        <div className="flex items-center gap-5">
+          <img
+            src={logo}
+            alt="Company Logo"
+            width="100"
+            height="100"
+            className="object-contain mr-auto"
+          />
+          <h2 className="text-3xl font-bold text-black">CV Booster</h2>
+        </div>
+        <div>
+          <ul className="flex gap-8 mr-16 text-[18px] text-gray-400">
+            <Link to="/home">
+              <li className="hover:text-blue-400 border-white cursor-pointer">
+                Trang chủ
+              </li>
+            </Link>
+            <Link to="/cvgenerate">
+              <li className="hover:text-blue-400 border-white cursor-pointer">
+                Tạo CV
+              </li>
+            </Link>
+            <Link to="/cvsamples">
+              <li className="hover:text-blue-400 border-white cursor-pointer">
+                Mẫu CV
+              </li>
+            </Link>
+            <Link to="/cvtips">
+              <li className="hover:text-blue-400 border-white cursor-pointer">
+                Mẹo viết CV
+              </li>
+            </Link>
+            <Link to="/faq">
+              <li className="hover:text-blue-400 border-white cursor-pointer">
+                FAQ
+              </li>
+            </Link>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Nvbar;
