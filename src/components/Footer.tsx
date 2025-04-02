0222b0372be76d672f887486df9423dc935c738a
TinMH <tinhongmai1012@gmail.com>
2025-04-02 17:59:40 +0700

    Add header and  footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,38 +1,42 @@
 import React from "react";
-import logo from "../images/logo_dsc.png";
 const Footer = () => {
   return (
-    <div>
-      <footer className="footer sm:footer-horizontal bg-blue-400 text-base-content px-10 py-4">
-        <aside>
-          <img
-            src={logo}
-            alt="Company Logo"
-            width="50"
-            height="50"
-            className="object-contain"
-          />
-        </aside>
-        <nav className="flex gap-4 ml-auto text-black">
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.facebook.com/hcmute.dsc"
-            className="link link-hover"
-          >
-            Fanpage
-          </a>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://github.com/hcmute-dsc"
-            className="link link-hover"
-          >
-            Github
-          </a>
-        </nav>
-      </footer>
-    </div>
+    <footer className=" footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
+      <nav>
+        <h6 className="footer-title">Services</h6>
+        <a className="link link-hover">Branding</a>
+        <a className="link link-hover">Design</a>
+        <a className="link link-hover">Marketing</a>
+        <a className="link link-hover">Advertisement</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Company</h6>
+        <a className="link link-hover">About us</a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.facebook.com/hcmute.dsc"
+          className="link link-hover"
+        >
+          Fanpage
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/hcmute-dsc"
+          className="link link-hover"
+        >
+          Github
+        </a>
+        <a className="link link-hover">Press kit</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Legal</h6>
+        <a className="link link-hover">Terms of use</a>
+        <a className="link link-hover">Privacy policy</a>
+        <a className="link link-hover">Cookie policy</a>
+      </nav>
+    </footer>
   );
 };
 export default Footer;
